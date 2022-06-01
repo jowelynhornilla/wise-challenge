5ba84f8ccdc4a73aca0351ec2c55cf8a2be9a60e
jowelynhornilla <jowelyn.hornilla@98labs.com>
2022-06-02 01:01:46 +0800

    initail commit

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -64,7 +64,7 @@ export const Dashboard: FC = () => {
   const handleSearchChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    setStartBlock(parseInt(e.target.value, 10) || 0);
+    setStartBlock(e.target.value ? parseInt(e.target.value, 10) : 0);
   };
 
   const handleSort = (key: string) => {
